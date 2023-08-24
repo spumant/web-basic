a7ccee2af17648cba58db3a4230a9f293e0ea1a2
spumant <253383985@qq.com>
2023-08-24 10:50:38 +0800

    模板语法

---
+++ 05_Vue/vue-base/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="msg">
+    测试
+  </div>
+  <button :disabled="isButtonDisabled">Button</button>
+  <div v-bind="objectOfAttrs"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "active",
+      isButtonDisabled: true,
+      objectOfAttrs: {
+        id: "aid",
+        class: "alcass"
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped></style>
